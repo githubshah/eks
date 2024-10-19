85f3b2026f79946fbab5dd974e7da073f81ac897
Shaid Hussain <shaid.hussain721@gmail.com>
2024-10-19 22:12:10 +0530

    done

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
+kubectl delete deployments --all
+kubectl delete deployments --all -n kube-system
+kubectl delete ingress --all
+minikube addons disable ingress
+kubectl get deployments
+kubectl get ingress
+helm uninstall api-b-helm
+
+Deploy service B
 minikube start
+minikube addons enable ingress
 
-Deploy Resporce B
-
-cd under resource-b folder where Dockerfile reside.
+cd under resource-b folder where Dockerfile file reside.
 eval $(minikube docker-env)
 docker build -t resource-b-image:latest .
 
 cd under resource-b/api-B-helm folder where values.yaml reside.
-helm install deployment-b .
+helm install api-b-helm .
 
 for testing purpose
-kubectl port-forward svc/deployment-b-api-b-helm 8081:8080
+kubectl port-forward svc/<service-name> 8081:8080
 Verify
 http://localhost:8081
+
+
+minikube tunnel
+
+sudo vim /etc/hosts
+echo "127.0.0.1 resource-b-domain.local" | sudo tee -a /etc/hosts
+ 
+
 
 ..
 
@@ -35,13 +51,14 @@ helm plugin install https://github.com/komodorio/helm-dashboard.git
 helm dashboard --port 8081
 
 
-
-Once forward testing done then
-minikube addons enable ingress
 minikube tunnel
 helm install my-nginx ingress-nginx/ingress-nginx --set controller.publishService.enabled=true
 
 echo "$(minikube ip) a.local" | sudo tee -a /etc/hosts
+
+
+helm install api-b-helm .
+helm uninstall api-b-helm
 
 
 
--- resource-b/src/main/java/com/example/controller/MyController.java
+++ resource-b/src/main/java/com/example/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,28 @@ public class MyController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<Object> hello() {
-        return restTemplate.getForEntity("https://abusiveexperiencereport.googleapis.com/$discovery/rest?version=v1", Object.class);
+        // Fetch the response from the external service
+        ResponseEntity<Object> response = restTemplate.getForEntity(
+                "https://abusiveexperiencereport.googleapis.com/$discovery/rest?version=v1", Object.class);
+
+        // Return a new ResponseEntity with the same body and status
+        // Make sure to avoid copying headers if they are problematic
+        return ResponseEntity.status(response.getStatusCode())
+                .body(response.getBody());
+    }
+
+    @GetMapping("/apple")
+    public String hello1() {
+        // Fetch the response from the external service
+        return "apple";
+    }
+
+    @GetMapping("/mango")
+    public ResponseEntity<Object>  hello2() {
+        // Fetch the response from the external service
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("mango");
     }
 }
